7170f573b759656ab85e0b0738deb2ad497e59c0
Matheus Paes Maciel <matheus_maciel@acad.ftec.com.br>
2020-11-06 18:27:00 -0300

    Inicio da crição do componente de dialogs

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule,LOCALE_ID } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,15 +11,16 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSnackBarModule } from '@angular/material/snack-bar';
-import {HttpClientModule} from '@angular/common/http';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import {MatFormFieldModule}from '@angular/material/form-field'
-import {MatInputModule}from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatDialogModule } from '@angular/material/dialog'
 
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
@@ -34,6 +35,7 @@ import { registerLocaleData } from '@angular/common';
 import localept from '@angular/common/locales/pt';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+import { DialogComponent } from './components/plugins/dialog/dialog.component';
 
 registerLocaleData(localept);
 
@@ -51,7 +53,8 @@ registerLocaleData(localept);
     ProductReadComponent,
     ProductRead2Component,
     ProductUpdateComponent,
-    ProductDeleteComponent
+    ProductDeleteComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
@@ -69,11 +72,12 @@ registerLocaleData(localept);
     MatFormFieldModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    MatDialogModule
   ],
   providers: [{
-    provide:LOCALE_ID,
-    useValue:'pt-BR'
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
   }],
   bootstrap: [AppComponent]
 })
